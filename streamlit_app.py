bf21f742750dcfb5885625ed48923b340157ff6f
Harrison Kennedy <108233996+harrison-kennedy@users.noreply.github.com>
2022-06-26 13:51:22 -0400

    Update streamlit_app.py
    test indentions

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,11 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
+
+
+
+
+
 streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
