1eb7b43b6acc54456299e46cc00924ec5bdb4a39
Harrison Kennedy <108233996+harrison-kennedy@users.noreply.github.com>
2022-06-28 08:57:30 -0400

    Update streamlit_app.py
    added json object to streamlit text

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,4 +31,4 @@ streamlit.header("Fruityvice Fruit Advice!")
 #New Section to display fruityvice
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
